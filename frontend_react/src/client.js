be14b4fd07a5f7acf4959787f84b5316e7a89ea8
Ed Melech <mr.melech@gmail.com>
2022-12-03 00:52:21 +0000

    set up env file

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,9 +2,13 @@ import { SanityClient } from "@sanity/client";
 import { ImageUrlBuilder } from "@sanity/image-url/lib/types/builder";
 
 export const client = sanityClient({
-  projectId: '',
-  dataset: '',
-  apiVersion: '',
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  dataset: 'production',
+  apiVersion: '2022-02-01',
   useCdn: true,
-  token: '',
-})
+  token: process.env.REACT_APP_SANITY_TOKEN,
+});
+
+const builder = ImageUrlBuilder(client)
+
+export const urlFor = (source) => builder.image(source)
