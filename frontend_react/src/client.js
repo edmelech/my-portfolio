e43577a963ca8e6ce12df553a70310e6fcf44edc
Ed Melech <mr.melech@gmail.com>
2022-12-05 16:27:19 +0000

    create about components using Sanity

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -1,7 +1,7 @@
-import { SanityClient } from "@sanity/client";
-import { ImageUrlBuilder } from "@sanity/image-url/lib/types/builder";
+import  SanityClient from "@sanity/client";
+import  ImageUrlBuilder from "@sanity/image-url";
 
-export const client = sanityClient({
+export const client = SanityClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2022-02-01',
