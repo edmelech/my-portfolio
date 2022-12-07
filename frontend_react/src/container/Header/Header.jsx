6b04f952d8411d14dab598c8e4bd8fa4c2ee7ae6
Ed Melech <mr.melech@gmail.com>
2022-12-07 19:03:19 +0000

    implement higher order componenets

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-// import { AppWrap } from '../../wrapper'; 
+import { AppWrap } from '../../wrapper'; 
 import { images } from '../../constants';
 import './Header.scss';
 
