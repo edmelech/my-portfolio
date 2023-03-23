9b2da42a36431eea063c272302c83b494a28738f
Ed Melech <mr.melech@gmail.com>
2023-03-23 16:57:55 +0000

    remove setToggle from Navigation dots componenet

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -9,7 +9,6 @@ const NavigationDots = ({ active }) => {
             key={item + index}
             className="app__navigation-dot"
             style={active === item ? { backgroundColor: '#313BAC' } : { }}
-            onClick={() => setToggle(false)}
           />
       ))}
     </div>
