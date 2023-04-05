ee902ec61259bc44316a5b3a6c295b5ae434c662
Ed Melech <mr.melech@gmail.com>
2023-04-05 20:37:39 +0100

    add skills icons to componenet via map

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -38,9 +38,14 @@ const Skills = () => {
               whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5}}
               className='app__skills-item app__flex'
-              key={skills.name}
+              key={skill.name}
             >
+              <div className='app__flex' style={{backgroundColor: skill.bgColor}}>
+                <img src={urlFor(skill.icon)} alt={skill.name} />
+              </div>
+              <p classname="p-text">{skill.name}
 
+              </p>
             </motion.div>
           ))}
         </motion.div>
