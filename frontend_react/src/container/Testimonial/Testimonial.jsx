53cb2431833eee21f09e45c6c39b9894977626c6
Ed Melech <mr.melech@gmail.com>
2023-04-20 18:46:15 +0100

    fix errors on testimonial componenet

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -28,10 +28,25 @@ const Testimonial = () => {
     });
    }, []);
 
-
+   const test = testimonials[currentIndex]
 
   return (
-    <div>Testimonial</div>
+    <>
+      {testimonials.length && (
+        <>
+          <div className='app__testimonial-item app__flex'>
+            <img src={urlFor(test.imgurl)} alt={test.name} />
+            <div className='app__testimonial-content'>
+              <p className='p-text'>{test.feedback}</p>
+              <div>
+                <h4 className='bold-text'>{test.name}</h4>
+                <h5 className='p-text'>{test.company  }</h5>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
   )
 }
 
