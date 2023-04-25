2c1b4a67d6180606afc6ccc3e8687060d5e52e76
Ed Melech <mr.melech@gmail.com>
2023-04-25 12:59:03 +0100

    add brands to testimonials page

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -11,6 +11,9 @@ const Testimonial = () => {
   const [testimonials, setTestimonials] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0)
 
+  const handleClick = (index) => (
+    setCurrentIndex(index) 
+  )
 
   useEffect(() => {
     const query = '*[_type == "testimonials"]';
@@ -46,11 +49,28 @@ const Testimonial = () => {
               </div>
             </div>
           </div>
-          <div>
-            
+          <div className='app__testimonial-btns app__flex'>
+            <div className='app__flex' onClick={() => handleClick(currentIndex === 0 ? testimonials.length -1 : currentIndex - 1)}>
+              <HiChevronLeft />
+            </div>
+            <div className='app__flex' onClick={() => handleClick(currentIndex === testimonials.length -1 ? 0 : currentIndex + 1)}>
+              <HiChevronRight />
+            </div>
           </div>
         </>
       )}
+
+      <div className='app__testimonials-brands app__flex'>
+        {brands.map((brand) => (
+          <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            key={brand.id}
+          >
+            <img src={urlFor(brand.imgUrl)} alt={brand.name}/>
+          </motion.div>
+        ))}
+      </div>
     </>
   )
 }
